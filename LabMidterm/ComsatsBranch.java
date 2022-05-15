93eb2abfd64c81b76b5eea42e61b8a529e3ab574
Muhammad Ahmad <m_ahmad329@yahoo.com>
2022-05-16 01:03:55 +0500

    cont

--- LabMidterm/ComsatsBranch.java
+++ LabMidterm/ComsatsBranch.java
@@ -6,7 +6,7 @@ public class ComsatsBranch extends IslamabadBranch {
 
     public ComsatsBranch(){
         super();
-        super.branch = "FAST";
+        super.branch = "FAST Comsats";
 
     }
 
